dcc030acf043fe329bc24ba92641a7eaa8d2b38e
Your Name <your.email@example.com>
2025-02-13 00:34:25 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
+});
